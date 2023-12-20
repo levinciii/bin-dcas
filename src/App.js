f4604c62ef2da4f2137ee5d6c89304e7c6b79020
Binzu <33591057+levinciii@users.noreply.github.com>
2023-12-21 00:47:02 +0800

    Add Pages and Components
    - Index.js has been added to Pages folder.
    - Navbar.js has been added to Components folder.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Pages 
+import Index from './pages/Index'
+
+// Components
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Index />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <div className="index">
+            <h2>Home</h2>
+        </div>
+    )
+}
+
+export default Index
