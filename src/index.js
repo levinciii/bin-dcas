a1f517158c9f9e106176dfdad126260ed71adda5
Binzu <33591057+levinciii@users.noreply.github.com>
2024-01-12 19:15:16 +0800

    Update 'index.js'
    - add the new path of AuthContextProvider.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { PatientsContextProvider } from './context/PatientsContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <PatientsContextProvider>
-        <App />
-    </PatientsContextProvider>
+    <AuthContextProvider>
+        <PatientsContextProvider>
+            <App />
+        </PatientsContextProvider>
+    </AuthContextProvider>
 );
