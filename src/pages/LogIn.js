51ac7a39f06e0ca7fdd65f22e070973bf579bdba
Binzu <33591057+levinciii@users.noreply.github.com>
2024-01-15 21:01:21 +0800

    Add useLogIn.js and Update LogIn.js
    - create useLogIn hook in order for the user to login to the system.
    - update LogIn form and implement the new hook that was added.

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { useLogIn } from '../hooks/useLogIn'
 
 const LogIn = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { login, isLoading, error } = useLogIn()
+
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await login(email, password)
     }
 
     return (
-        <form className="signup" onSubmit={handleSubmit}>
+        <form className="login" onSubmit={handleSubmit}>
             <h3>Log In</h3>
             <label>Email:</label>
             <input type="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
             <label>Password:</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
-            <button>Log In</button>
+            <button disabled={isLoading}>Log In</button>
+            { error && <div className="error">{error}</div> }
         </form>
     )
 }
