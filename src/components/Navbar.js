025eb9f4523dad9daba75bc5707f20dafc503927
Binzu <33591057+levinciii@users.noreply.github.com>
2024-01-16 16:33:26 +0800

    Update Navbar.js and AuthContext.js
    - display email of the user when logged in.
    - make the JSON Web Token and user details retained whenever you refresh the page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
 import { useLogOut } from '../hooks/useLogOut'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
     const { logout } = useLogOut()
+    const { user } = useAuthContext()
 
     const handleClick = () => {
         logout()
@@ -13,13 +15,18 @@ const Navbar = () => {
             <div className="container">
                 <Link to="/"><h1>Patients</h1></Link>
                 <nav>
-                    <div>
-                        <button onClick={handleClick}>Log Out</button>
-                    </div>
+                    {user && (
+                        <div>
+                            <span>{user.email}</span>
+                            <button onClick={handleClick}>Log Out</button>
+                        </div>
+                    )}
+                    {!user && (
                     <div>
                         <Link to="/login">Log In</Link>
                         <Link to="/signup">Sign Up</Link>
                     </div>
+                    )}
                 </nav>
             </div>
         </header>
