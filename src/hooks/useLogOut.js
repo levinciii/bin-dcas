2295514d1a0363a0807096bfbcfdbecac46be5d5
Binzu <33591057+levinciii@users.noreply.github.com>
2024-01-19 23:07:35 +0800

    Update useLogOut.js and index.css
    - to remove whenever you logged out the system.
    - fix the spacing of the email address and logout button.

--- src/hooks/useLogOut.js
+++ src/hooks/useLogOut.js
@@ -1,13 +1,16 @@
 import { useAuthContext } from './useAuthContext'
+import { usePatientsContext } from './usePatientsContext'
 
 export const useLogOut = () => {
     const { dispatch } = useAuthContext()
+    const { dispatch: patientsDispatch } = usePatientsContext()
 
     const logout = () => {
         // Remove User from Storage
         localStorage.removeItem('user')
         // Dispatch LogOut Action
         dispatch({type: 'LOGOUT'})
+        patientsDispatch({type: 'SET_PATIENTS', payload: null})
     } 
 
     return { logout }
