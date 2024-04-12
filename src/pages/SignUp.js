cbc173242dd46bb7f94fae213fa7211d77882af7
BINzu <calmavincee@gmail.com>
2024-04-12 15:14:20 +0800

    Update SignUp Form
    - update SignUp form's css

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 import { useSignUp } from '../hooks/useSignUp'
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 const SignUp = () => {
+    const [fname, setFName] = useState('')
+    const [mname, setMName] = useState('')
+    const [lname, setLName] = useState('')
+    const [birthdate, setBirthdate] = useState('')
+    const [age, setAge] = useState('')
+    const [phone_num, setPhoneNum] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const { signup, isLoading, error } = useSignUp()
@@ -15,10 +23,53 @@ const SignUp = () => {
     return (
         <form className="signup" onSubmit={handleSubmit}>
             <h3>Sign Up</h3>
-            <label>Email:</label>
-            <input type="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <label>First Name</label>
+                        <input type="text" onChange={(e) => setFName(e.target.value)} value={fname}/>
+                    </Col>
+                    <Col>
+                        <label>Middle Name</label>
+                        <input type="text" onChange={(e) => setMName(e.target.value)} value={mname}/>
+                    </Col>
+                    <Col>
+                        <label>Last Name</label>
+                        <input type="text" onChange={(e) => setLName(e.target.value)} value={lname}/>
+                    </Col>
+                </Row>
+            </Container>
+            
+            <Container>
+                <Row>
+                    <Col xs={9}>
+                        <label>Birth Date</label>
+                        <input type="date" onChange={(e) => setBirthdate(e.target.value)} value={birthdate}/>
+                    </Col>
+                    <Col>
+                        <label>Age</label>
+                        <input type="text" onChange={(e) => setAge(e.target.value)} value={age}/>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <label>Phone Number</label>
+                        <input type="text" onChange={(e) => setPhoneNum(e.target.value)} value={phone_num}/>
+                    </Col>
+                    <Col>
+                        <label>Email</label>
+                        <input type="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    </Col>
+                </Row>
+            </Container>
+        
             <label>Password:</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+            
             <button disabled={isLoading}>Sign Up</button>
             { error && <div className="error">{error}</div> }
         </form>
